a1a11bd3f7420f8fee41d2cff7a35deb2de02c2a
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-02-13 02:51:49 +0900

    짝수와 홀수
    짝수와 홀수

---
+++ Solution7.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "Even";
+        if(num%2==0){
+            return answer;
+        }
+        else{
+            answer = "Odd";
+            return answer;
+        }
+        
+       
+    }
+}
