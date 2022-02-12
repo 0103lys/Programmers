38997de4ce0546779d4052afaf4461831c8b9dcb
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-02-12 22:48:54 +0900

    직사각형 별찍기
    직사각형 별찍기

--- Solution1.java
+++ Solution1.java
@@ -6,7 +6,7 @@ class Solution {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        for(int i=0; i<b; i++){
+        for(int i= 0; i<b; i++){
             for(int j=0; j<a; j++){
                 System.out.print("*");
             }
