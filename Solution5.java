ef84b32d02593d173decb8d0e88c05b02b56d815
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-02-15 21:49:01 +0900

    평균 구하기
    평균 구하기

--- Solution5.java
+++ Solution5.java
@@ -1,11 +1,10 @@
 class Solution {
     public double solution(int[] arr) {
-        int[] array = new int[arr.length];
         double answer = 0;
         for(int i=0; i<arr.length; i++){
             answer += arr[i];
         }
-        answer = answer / arr.length;
+        answer = answer/ arr.length;
         
        
         return answer;
