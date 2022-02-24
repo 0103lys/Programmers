b3a90774e3ec937dad37af821e49a194f5f3194e
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-02-25 02:44:54 +0900

    문자열 내림차순으로 배치하기
    문자열 내림차순으로 배치하기

--- Solution19.java
+++ Solution19.java
@@ -2,7 +2,9 @@ import java.util.Arrays;
 import java.util.Collections;
 class Solution {
     public String solution(String s) {
-        String[] arr = s.split("");
+        String[] arr = s.split(""); // 문자열 spilt
+        
+        // 문자열 배열 역순으로 정렬하기
         Arrays.sort(arr, Collections.reverseOrder());
         String answer = "";
         for(String string : arr){
