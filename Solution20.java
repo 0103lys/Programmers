2fac9dafffc08f190fe8db4b024049f1cea2b4cf
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-02-25 02:43:08 +0900

    문자열 다루기 기본
    문자열 다루기 기본

--- Solution20.java
+++ Solution20.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean solution(String s) {
         boolean answer = true;
+        
+        // 정규식 4자리 혹은 6자리인 숫자로만 구성되어있을 때 -> true
         if(s.matches("\\d{4}") || s.matches("\\d{6}")){
             return answer;
     
