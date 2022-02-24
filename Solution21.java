384918039878047b3b8d93bc2b4663dc92fb2102
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-02-25 03:51:18 +0900

    서울에서 김서방 찾기
    서울에서 김서방 찾기

---
+++ Solution21.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        for(int cnt=0; cnt< seoul.length; cnt++){
+            if(seoul[cnt].equals("Kim")){
+                answer += "김서방은 " +cnt+"에 있다";
+            }
+        }
+        return answer;
+    }
+}
