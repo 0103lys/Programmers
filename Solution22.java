ba6d55be64762c410ff2622e486385bab352faa4
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-02-25 13:13:36 +0900

    문자열 내 p와 y의 개수
    문자열 내 p와 y의 개수

---
+++ Solution22.java
@@ -0,0 +1,25 @@
+class Solution {
+    boolean solution(String s) {
+        int i=0, j=0, cnt;
+        String[] arr = s.split("");
+        boolean answer = true;
+
+        for(cnt=0; cnt<arr.length; cnt++){
+            if(arr[cnt].equals("p")||arr[cnt].equals("P")){
+                i++;
+            }
+            else if(arr[cnt].equals("y")||arr[cnt].equals("Y")){
+                j++;
+            }
+        }
+        if(i==j){
+            return answer;
+        }else {
+            answer = false;
+            return answer;
+
+        }
+
+    
+    }
+}
