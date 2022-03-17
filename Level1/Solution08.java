91ba36842e7b3a8912388ecd5b36a0e499900372
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-03-17 18:25:33 +0900

    이상한 문자 만들기
    이상한 문자 만들기

---
+++ Level1/Solution08.java
@@ -0,0 +1,29 @@
+// 이상한 문자 만들기
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] result = s.split(""); // 공백을 기준으로 나누기
+        int index = 0;
+        StringBuffer word = new StringBuffer();
+        
+        
+         for (int i = 0; i < result.length; i++) {
+            if (result[i].equals(" ")) {	// 공백일 때 index를 0으로 되돌리기
+                index = 0;
+            } else if (index % 2 == 0) {	// 짝수번째일 때 대문자로 바꾸기
+                result[i] = result[i].toUpperCase();
+                index++;
+            } else if (index % 2 != 0) {	// 홀수번째일 때 소문자로 바꾸기
+                result[i] = result[i].toLowerCase();
+                index++;
+            }
+            word.append(result[i]);
+            
+            
+        }
+        answer = word.toString();
+        return answer;
+      
+        
+    }
+}
