352d61b17c167fe69ae2883be2939bd905a6c8e4
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-03-18 23:21:46 +0900

    가운데 글자 가져오기
    가운데 글자 가져오기

---
+++ Level1/Solution29.java
@@ -0,0 +1,15 @@
+// 가운데 글자 가져오기
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        if(s.length()%2==0){
+            answer += s.charAt((s.length()/2)-1);
+            answer += s.charAt(s.length()/2);
+        }else {
+            answer += s.charAt(s.length()/2);
+        }
+
+
+       return answer;
+    }
+}
